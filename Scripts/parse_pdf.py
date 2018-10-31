864d140e85a25dcd4caf11f17f6e03ccd766b604
Leonore Guillain <leonore.guillain@epfl.ch>
2018-10-31 14:27:06 +0100

    Adding script to extract txt

---
+++ Scripts/parse_pdf.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+import time
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str, dest='path', default='papers-import',  help='specifies the path to input dir')
+parser.add_argument('-o', type=str, dest='output', default='data',  help='specifies the path to output dir')
+
+
+rootdir = parser.parse_args().path
+output = parser.parse_args().output
+
+print('[Info] Parsing from {}'.format(rootdir))
+TOTEXT = 'pdftotext '
+PARAMS = ' -nopgbrk -eol mac'
+
+j = 0
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        path = os.path.join(subdir, file)+' '
+        path_out = os.path.join(output, subdir[len(rootdir)+1:].replace('/', '_')+file[file.find('/'):-len('.pdf')]+'.txt')
+        if '.pdf' in path:
+            j += 1
+            os.system(TOTEXT + path + path_out + PARAMS)
+            sys.stdout.write("\r[Info] Parsed %d documents" % j)
+            sys.stdout.flush()
